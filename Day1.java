fb6fbcfd714962207892a4ff46e1b69f0371e6f4
24satyam <66178378+24satyam@users.noreply.github.com>
2024-07-20 10:20:40 +0530

    added recursive and stack approach

--- Day1.java
+++ Day1.java
@@ -43,3 +43,51 @@ public static String reverseWord(String str) {
        return new String (a) ;
     }
 
+
+// Approach 3 👍
+
+// Using Recursion (index ko bdhana ya ghatana hoga next call k time index+1 aesa kuch )
+// Jab start end se bada ho jayega toh return krdo yahi hmara base case hoga 
+// Ab ek chij smjho ki jb tm temp ki help se ek element ko reverse kr skte ho to leap of faith lo ki agey bhi ye hota rhega qnki akhir mai fnxn toh wahi call kr rhe ho , isiliye toh jo tm   while(start<=end ) likh rhe the uske jga recursive call krdo start+1 aur end -1 krke 
+
+// (same chij stack se bhi kr skte ho )
+
+// O(n). The recursion goes through each element once, so it’s linear
+// Auxiliary Space Complexity: O(n) for non in-place, O(log n) for in-place (due to recursion stack)
+// Aur ek chij smjho ki iss chij mai hmlog jo bhi swap kr rhe hai wo in place hai i.e jagah whi reh rhi hai (inplace ka mtlb hi yahi hai 🤣 ) 
+// (But stack mai aesa nhi hoga udhar non -inplace hoga kahe ki stack 🙂)
+static void reverseArray(int arr[], int start, int end)
+    {
+        int temp;
+        if (start >= end)
+            return;
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArray(arr, start + 1, end - 1);
+    }
+
+// Approach 4 👍
+
+// Push krke pop krne pr ulta sequence mai milega 
+
+public static void reverseArrayUsingStack(int[] arr)
+    {
+        Stack<Integer> stack = new Stack<>();
+
+        // Push elements onto the stack
+        for (int element : arr) {
+            stack.push(element);
+        }
+
+        // Pop elements from the stack to reverse the array
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = stack.pop();
+        }
+    }
+
+
+
+
+
+
